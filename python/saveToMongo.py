ccc0c66006a88b486d4732430881eff5fe433874
xnieamo <xiaomao_ding@yahoo.com>
2015-09-05 04:32:25 -0400

    python to mongo file

---
+++ python/saveToMongo.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 05 04:16:12 2015
+
+@author: Xiaomao
+"""
+import json
+import sys
+from pymongo import MongoClient
+
+def main(argv):
+    filename = sys.argv[1]
+    json_data = open(filename).read()
+    data = json.loads(json_data)
+    
+    mongoClient = MongoClient()
+    db = mongoClient['testDb']
+    
+    collection = db['testColl']
+    posts = db.posts
+    
+    patientCount = 0;
+    while patientCount < 1:
+        posts.insert_one(data[patientCount])
+        patientCount += 1
+    
+if __name__ == "__main__":
+   main(sys.argv[1:])

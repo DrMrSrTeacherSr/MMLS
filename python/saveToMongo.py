e1a3e2dd9be969e62a1babda78f11f5b4c2d8a59
xnieamo <xiaomao_ding@yahoo.com>
2015-09-05 05:38:14 -0400

    another attempt at $

--- python/saveToMongo.py
+++ python/saveToMongo.py
@@ -11,7 +11,7 @@ from pymongo import MongoClient
 def main(argv):
     filename = sys.argv[1]
     json_data = open(filename).read()
-    data = json.loads(json_data)
+    data = json.loads(json_data, object_hook=remove_dollarsign)
     
     mongoClient = MongoClient()
     db = mongoClient['testDb']
@@ -21,13 +21,21 @@ def main(argv):
     
     patientCount = 0;
     while patientCount < 1:
-        singleData = data[patientCount]
-        keySet = singleData.keys()
-        for key in keySet:
-            if key.find('$') > -1:
-                key.replace('S', '')
+#        singleData = data[patientCount]
+#        keySet = singleData.keys()
+#        for key in keySet:
+#            if key.find('$') > -1:
+#                key.replace('S', '')
         posts.insert_one(data[patientCount])
         patientCount += 1
+
+def remove_dollarsign(obj):
+    for key in obj.keys():
+        new_key = key.replace('$','')
+        if new_key!=key:
+            obj[new_key] = obj[key]
+            del obj[key]
+    return obj
     
 if __name__ == "__main__":
    main(sys.argv[1:])

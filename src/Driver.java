8dbb592ba77057dfd5ce5d7d993c5b8968e64d3c
xnieamo <xiaomao_ding@yahoo.com>
2015-09-04 22:19:11 -0400

    HAHAHA

--- src/Driver.java
+++ src/Driver.java
@@ -5,6 +5,7 @@ public class Driver {
 		System.out.println("Hello World");
 		System.out.println("This is an intellij test");
 		System.out.println("The cake is a lie");
+		System.out.println("My edit");
 	}
 	
 	

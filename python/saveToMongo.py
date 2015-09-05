1b4c970a268ed792a6b86f4d18c00175ed20d66f
xnieamo <xiaomao_ding@yahoo.com>
2015-09-05 12:00:33 -0400

    adsf

--- python/saveToMongo.py
+++ python/saveToMongo.py
@@ -38,10 +38,10 @@ def main(argv):
     print(data.keys()) 
             
     try:
-        if isinstance(data[key], dict):
-            formatDict = getFormatDict(data[key])
-        else:
-            formatDict = getFormatDict(data)
+#        if isinstance(data[key], dict):
+#            formatDict = getFormatDict(data)
+#        else:
+        formatDict = getFormatDict(data[key])
         #print(formatDict)
         # Make all necessary formatting edits here
 #        formatDict = legalDict(data, formatDict)

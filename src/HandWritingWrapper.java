295a2cf43b08aafb6a548c3f5e72e0b80792181b
Cam <zawacki.cameron@gmail.com>
2015-09-05 23:35:11 -0400

    getters and setters for learning rate

--- src/HandWritingWrapper.java
+++ src/HandWritingWrapper.java
@@ -30,7 +30,7 @@ public class HandWritingWrapper {
 				BasicDBList image = (BasicDBList) temp.get("image");
 
 				
-				System.out.println(image.size());
+				System.out.println(image.get(0));
 				
 			}
 			

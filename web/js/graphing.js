f654f47de9b01a604f1dd53e0356f8d3af9815f0
root <skrulcik@andrew.cmu.edu>
2015-09-05 20:40:30 -0400

    Added Number Line chart for 1D numeric data

--- web/js/graphing.js
+++ web/js/graphing.js
@@ -84,9 +84,7 @@ function fieldTypes(arrayToPlot) {
 	return typeList;
 }
 function validGraphsForData(data) {
-	console.log("Pre-cleanse: " + data);
 	data = cleanTypes(data);
-	console.log("after-cleanse: " + data);
 	var types = fieldTypes(data);
 	if (!types || types.length == 0) {
 		console.error("Yo give me better data, that's not an array");
@@ -107,7 +105,6 @@ function validGraphsForData(data) {
 }
 function presentGraphOptions(data) {
 	$('#modal-chooser-options').html('');
-	console.log(validGraphsForData(data));
 	$.each(validGraphsForData(data), function() {
 		var buttonHTML = '<div><button class="light-blue darken-2 waves-effect waves-light white-text btn-flat"'
 		buttonHTML += ' onclick="presentGraphType(\''+this+'\',['+data+'])">';
@@ -118,33 +115,93 @@ function presentGraphOptions(data) {
 	$('#modal-chooser').openModal();
 }
 function presentGraphType(type, data) {
-	alert("Success! " + type);
+	$('#modal-chooser').closeModal();
+	if (type == Graph.types.NUM_LINE) {
+		popUpNumberLine(data);
+	} else {
+		alert("Sorry, that graph type is coming soon :)");
+	}
 }
 
 
 /*****************************************************************
 * Graphics
 *****************************************************************/
-function BarDataMax(a, b) {
-	if(a.data < b.data) {
-		return b.data;
-	}
-	return a.data;
-}
-function BarDataArrMax(arr) {
-	return d3.max(arr.map(function(bd){return bd.data}));
-}
+// function BarDataMax(a, b) {
+// 	if(a.data < b.data) {
+// 		return b.data;
+// 	}
+// 	return a.data;
+// }
+// function BarDataArrMax(arr) {
+// 	return d3.max(arr.map(function(bd){return bd.data}));
+// }
 
-/* Chart sizer encapsulates size and scaling of a chart into 1 */
-function CreateChartSizer(data,width, k_label, k_data) {
+// /* Chart sizer encapsulates size and scaling of a chart into 1 */
+// function CreateChartSizer(data, width, k_label, k_data) {
+// 	return {
+// 		data: data,
+// 		width: width,
+// 		get label_width()   { return this.width * k_label},
+// 		get data_padding()  { return this.width * k_data },
+// 		get max_bar_width() { return this.width - this.label_width - this.data_padding},
+// 		get scale() { return d3.scale.linear().domain([0, BarDataArrMax(this.data)]).range([0, this.max_bar_width]) }
+// 	}
+// }
+
+function CreateGraphicalData(data, maxWidth) {
 	return {
 		data: data,
-		width: width,
-		get label_width()   { return this.width * k_label},
-		get data_padding()  { return this.width * k_data },
-		get max_bar_width() { return this.width - this.label_width - this.data_padding},
-		get scale() { return d3.scale.linear().domain([0, BarDataArrMax(this.data)]).range([0, this.max_bar_width]) }
+		width: maxWidth,
+		get max_value() { return d3.max(data) },
+		get min_value() { return d3.min(data) },
+		get range() { return this.max_value - this.min_value },
+		get scale() {
+			var offset = d3.max([this.range * 0.1, 1]);
+			var min = this.min_value - offset;
+			var max = this.max_value + offset;
+			var other = this.width;
+			var scaleTest = d3.scale.linear()
+						.domain([this.min_value - offset, this.max_value + offset])
+						.range([0, this.width]); 
+			var test1 = scaleTest(this.min_value);
+			var test2 = scaleTest(this.max_value);
+			var test3 = scaleTest(3.0);
+			return scaleTest;
+		}
 	}
+}
+
+function popUpNumberLine(data) {
+	$('#modal-graph-data').html('');
+	$('#modal-graph').openModal();
+	$('#modal-graph-data').append('<div class="center-align"><h3>Number Line</h3></div>')
+						  .append('<div id="modal-graph-data-number-line"></div>');
+
+	var width = $('#modal-graph').width() * 0.8;
+	var height = $('#modal-graph').height() * .5;
+	var svgContainer = d3.select('#modal-graph-data-number-line').append('svg')
+									.attr('width', width)
+                                    .attr('height', height);
+	var GraphInfo = CreateGraphicalData(data, width);
+    var xAxis = d3.svg.axis()
+					.scale(GraphInfo.scale)
+					.ticks(5)
+					.orient('bottom');
+	svgContainer.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height/2.0 + ")")
+    .call(xAxis)
+
+    var g = svgContainer.append("svg:g"); 
+	g.selectAll("scatter-dots")
+	  .data(data)
+	  .enter().append("svg:circle")
+	      .attr("cy", function (d) { return 0;} )
+	      .attr("cx", function (d) { return GraphInfo.scale(d); } )
+	      .attr("transform", "translate(0," + height/2.0 + ")")
+	      .attr("r", 5);
+
 }
 
 
@@ -153,26 +210,26 @@ function popUpGraph(data) {
 	var data_width = d3.select('#modal-graph-data').style('width').match(/\d+/)[0];
 	var data_sizer = CreateChartSizer(data, data_width, .25, .10);
 
-	var bar_containers = d3.select('#modal-graph-data')
-							.selectAll('div')
-							.data(data)
-							.enter().append('div')
-							.classed('bar_container', true);
-	// Add labels, then bars, then data
-	bar_containers.append('div')
-	    .classed('bar_label', true)
-	    .style('width', data_sizer.label_width)
-	    .style('height', '100%')
-	    .text(function(d) {return d.name;});
-	bar_containers.append('div')
-	    .classed('bar', true)
-	    .style('width',( function(d) { return data_sizer.scale(d.data) + 'px'; }))
-	    .style('height', '100%');
-	bar_containers.append('div')
-	 	.classed('bar_data', true)
-	 	.style('width', function(d) { return (data_sizer.width - data_sizer.scale(d.data) - data_sizer.label_width + data_sizer.data_padding) + 'px'})
-	 	.style('height', '100%')
-	    .text(function(d) { return d.data; });
+	// var bar_containers = d3.select('#modal-graph-data')
+	// 						.selectAll('div')
+	// 						.data(data)
+	// 						.enter().append('div')
+	// 						.classed('bar_container', true);
+	// // Add labels, then bars, then data
+	// bar_containers.append('div')
+	//     .classed('bar_label', true)
+	//     .style('width', data_sizer.label_width)
+	//     .style('height', '100%')
+	//     .text(function(d) {return d.name;});
+	// bar_containers.append('div')
+	//     .classed('bar', true)
+	//     .style('width',( function(d) { return data_sizer.scale(d.data) + 'px'; }))
+	//     .style('height', '100%');
+	// bar_containers.append('div')
+	//  	.classed('bar_data', true)
+	//  	.style('width', function(d) { return (data_sizer.width - data_sizer.scale(d.data) - data_sizer.label_width + data_sizer.data_padding) + 'px'})
+	//  	.style('height', '100%')
+	//     .text(function(d) { return d.data; });
 }
 
 
